cb9080fc903ff49faf22a97e5ae563c9e204508d
noandersonls <alljedicake@gmail.com>
2019-10-31 19:02:02 -0300

    Updates

---
+++ precache-manifest.929b6828fe56dc7442250b6d0b90a5d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370a6f7287bde897d752f121f7c7614e",
+    "url": "/congressbook/index.html"
+  },
+  {
+    "revision": "5db97ca7063804e6c84e",
+    "url": "/congressbook/static/css/main.6d7d62e2.chunk.css"
+  },
+  {
+    "revision": "46dd009f084e6ac1a170",
+    "url": "/congressbook/static/js/2.d1e256ae.chunk.js"
+  },
+  {
+    "revision": "5db97ca7063804e6c84e",
+    "url": "/congressbook/static/js/main.0e5b249a.chunk.js"
+  },
+  {
+    "revision": "a61e888c628dac5c6bcc",
+    "url": "/congressbook/static/js/runtime-main.9e5be7a1.js"
+  }
+]);
